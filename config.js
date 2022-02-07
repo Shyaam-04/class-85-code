4329c26972f2895219105dc7194fc1856e2173a6
Shyaam-04 <77397231+Shyaam-04@users.noreply.github.com>
2022-02-07 20:09:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDurpbENrhEgw0GPT9pekRG3-I3dF7mFcg",
+    authDomain: "storytellingapp-6c819.firebaseapp.com",
+    projectId: "storytellingapp-6c819",
+    storageBucket: "storytellingapp-6c819.appspot.com",
+    messagingSenderId: "882264594842",
+    appId: "1:882264594842:web:c1a6afa72ebbdf9b434223"
+  };
